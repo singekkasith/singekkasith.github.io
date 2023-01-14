14604ede057057588974ea98dfbddd65c605a409
Ekkasith S <ekkasith.dew@gmail.com>
2023-01-15 00:09:09 +0700

    Midterm & Bonus Question

--- product.js
+++ product.js
@@ -29,8 +29,8 @@ $(document).ready(function () {
 function addProduct() {
 
     let productObj = {
-        name: $('#products').val(),
         quantity: $('#qty').val(),
+        name: $('#products').val(),
         ppu: $('#ppu').val(),
         discount: $('#discount').val()
     }
@@ -40,15 +40,21 @@ function addProduct() {
     iProducts.push(productObj) 
     console.log(iProducts)
 
+    
     let index = iProducts.length - 1
     loadData()
+    
 }
 
 function deleteProduct(index){
     console.log("DELETE", index)
-    delete iProducts[index]
+
+    //delete iProducts[index] 
+
+    iProducts.splice(index, 1)  
 
     $('#data-product').html("")
+    console.log(iProducts)
     loadData()
 
 }
@@ -56,9 +62,12 @@ function deleteProduct(index){
 //For ClearProduct
 function clearProduct(){
     console.log("CLEAR ALL ITEMS")
-    for (let i in iProducts){
+    /*for (let i in iProducts){
         delete iProducts[i]
-    }
+    }*/
+
+    iProducts.splice(0, iProducts.length)
+    
     $('#productBody').html("")
     loadData()
 }
@@ -67,14 +76,16 @@ function loadData(){
     let allRows = ""
     let gross = 0
     let totDis = 0
+    
 
     for (let p in iProducts) {
+        
 
         let cellDelete =  `<td> <img src='delete-icon.png' width='25em' onclick='deleteProduct("${p}")' />` + "" + "</td>"
         let cellName = "<td>"  + iProducts[p].name + "</td>"
-        let cellQuantity = "<td>" + iProducts[p].quantity + "</td>"
-        let cellPPU = "<td>" + iProducts[p].ppu + "</td>"
-        let cellDiscount =  "<td>" + iProducts[p].discount + "</td>"
+        let cellQuantity = "<td>" + Number(iProducts[p].quantity) + "</td>"
+        let cellPPU = "<td>" + Number(iProducts[p].ppu).toFixed(2) + "</td>"
+        let cellDiscount =  "<td>" + Number(iProducts[p].discount).toFixed(2) + "</td>"
         let total = (iProducts[p].ppu * iProducts[p].quantity) - iProducts[p].discount
         
         gross += total
@@ -86,6 +97,7 @@ function loadData(){
         let cellTotal = `<td class="text-end">` + total.toFixed(2) + "</td>"
         let row = `<tr>${cellDelete}${cellQuantity}${cellName}${cellPPU}${cellDiscount}${cellTotal}</tr>`
         allRows += row
+        
     }
 
     $('#data-product').html(allRows)
@@ -97,4 +109,42 @@ function loadData(){
     let net = gross + vat
     $("#vat").html(vat.toFixed(2))
     $("#net").html(net.toFixed(2))
+
+    checkRedundant()
 }
+
+function checkRedundant(){
+    deleteOrder = []
+    //Bootleg Redundant Product Checking
+
+    for (let i = iProducts.length - 1; i >= 0 ; i--) {
+        for (let p in iProducts){
+
+            if (iProducts[p] != null){
+                let compareResult = iProducts[i].name.localeCompare(iProducts[p].name)
+                
+                if (compareResult == 0 && iProducts[i].ppu == iProducts[p].ppu && i != p){
+
+                    iProducts[p].quantity = Number(iProducts[p].quantity) + Number(iProducts[i].quantity)
+
+                    console.log(iProducts[i].name + " and " + iProducts[p].name + " is " +compareResult)
+                    console.log(iProducts[i].ppu + " == " + iProducts[p].ppu)
+                    console.log("Redundancy Detected!")
+
+                    iProducts[i].name = iProducts[i].name + " Checked"  // not repeat the items that is already checked.
+                    
+                    deleteOrder.push(i)
+                    console.log(deleteOrder)
+                } 
+            }   
+        }
+        
+    }
+
+    for (let d in deleteOrder){
+        console.log(deleteOrder[d])
+        deleteProduct(deleteOrder[d])
+    }
+    
+}
+
